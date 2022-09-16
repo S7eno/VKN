7da20a1ca21b04d09d6a0f77a31fe74b08c84fc8
Олександр Марюхно <113389249+S7eno@users.noreply.github.com>
2022-09-16 18:23:26 +0300

    Delete Lab3.py

--- Lab3.py
+++
@@ -1,7 +0,0 @@
-a = int(input ("Ведіть число № 1: "))
-b = int(input ("Ведіть число № 2: "))
-c = int(input ("Ведіть число № 3: "))
-import random
-for i in range(3):
-    a=random.randint(100,999)
-    print(a)
