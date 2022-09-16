f73ee0e469f8375830ee591d4646414fb6a73c0c
Олександр Марюхно <113389249+S7eno@users.noreply.github.com>
2022-09-16 18:23:02 +0300

    Add files via upload

---
+++ Lab3.py
@@ -0,0 +1,7 @@
+a = int(input ("Ведіть число № 1: "))
+b = int(input ("Ведіть число № 2: "))
+c = int(input ("Ведіть число № 3: "))
+import random
+for i in range(3):
+    a=random.randint(100,999)
+    print(a)
